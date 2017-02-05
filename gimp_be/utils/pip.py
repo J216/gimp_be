5ac5bd497e468f0a7b0804fd82d21c13ac27f7f4
Jared Haer <jaredhaer@gmail.com>
2017-02-04 23:24:11 -0600

    fixed imports

--- gimp_be/utils/pip.py
+++ gimp_be/utils/pip.py
@@ -1,5 +1,7 @@
+import subprocess, os, urllib, sys
 #install pip --windows only, I think--
 def installPIP():
+    import os, urllib,subprocess
     gimp_python=sys.executable[:-5]+'.exe'
     os.chdir(gimp_python[:-10])
     urllib.urlretrieve ("https://bootstrap.pypa.io/get-pip.py", "get-pip.py")
@@ -8,6 +10,7 @@ def installPIP():
 
 #install module using PIP install, provide module name as string
 def pipInstall(module):
+    import os, urllib, subprocess
     gimp_python=sys.executable[:-5]+'.exe'
     os.chdir(gimp_python[:-10])
     print "python -m pip install "+module
