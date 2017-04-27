bdb67eed4800a664dc847c80e70f7a4dee81638d
Jared Haer <jaredhaer@gmail.com>
2017-04-27 11:04:31 -0500

    added function to return array of random poins with in image added drawInkBlot() to draw ink blots added gridCenters() to return center points of N1 length and N2 height added tileMask()  added mask layer using current brush added drawAkuTree() to draw creepy looking samari jack aku tree add drawAkuForest() to draw forest of Aku trees

--- gimp_be/draw/draw.py
+++ gimp_be/draw/draw.py
@@ -1,6 +1,7 @@
 from gimpfu import gimp, pdb, SELECT_CRITERION_COMPOSITE
 import random, math
 from gimp_be.settings.brush_settings import *
+from gimp_be.image.layer import editLayerMask
 
 
 def drawRays(rays=32, rayLength=100, centerX=50, centerY=75):
@@ -211,4 +212,137 @@ def randomBlend():
     threshold, dither, x1, y1, x2, y2)
     pdb.gimp_blend(drawable, blend_mode, paint_mode, gradient_type, opacity, offset, repeat, reverse, supersample,
                    max_depth, threshold, dither, x1, y1, x2, y2)
+
+
+def randomPoints(num=12):
+    d = []
+    for x in range(num):
+        d.append(choice(range(boarder,image.width-boarder)))
+        d.append(choice(range(boarder,image.height-boarder)))
+    return d
+
+
+def drawInkBlot(option=''):
+    image=gimp.image_list()[0]
+    layer=pdb.gimp_image_get_active_layer(image)
+    if 'trippy' in option:
+        layer_copy = pdb.gimp_layer_copy(layer, 0)
+        pdb.gimp_image_add_layer(image, layer_copy,1)
+        randomBlend()
+        mask = pdb.gimp_layer_create_mask(layer,5)
+        pdb.gimp_image_add_layer_mask(image, layer,mask)
+        editLayerMask(1)
+    randomCircleFill(num=15,size=800)
+    brushColor(255,255,255)
+    randomCircleFill(num=50,size=100)
+    randomCircleFill(num=5,size=300)
+    brushColor(0)
+    randomCircleFill(num=20,size=600)
+    randomCircleFill(num=50,size=400)
+    randomCircleFill(num=100,size=100)
+    brushColor(255,255,255)
+    randomCircleFill(num=50,size=100)
+    brushColor(0)
+    drawable = pdb.gimp_image_active_drawable(image)
+    brushSize()
+    strokes=[random.randrange(0,image.width/2),random.randrange(0,image.height),random.randrange(0,image.width/2),random.randrange(0,image.height)]
+    pdb.gimp_smudge(drawable, random.choice([1,5,10,50,100]), len(strokes), strokes)
+    brushSize()
+    strokes=[random.randrange(0,image.width/2),random.randrange(0,image.height),random.randrange(0,image.width/2),random.randrange(0,image.height)]
+    pdb.gimp_smudge(drawable, random.choice([1,5,10,50,100]), len(strokes), strokes)
+    pdb.gimp_image_select_rectangle(image, 0, 0, 0, image.width/2, image.height)
+    non_empty = pdb.gimp_edit_copy(drawable)
+    pdb.gimp_edit_paste(drawable, 1)
+    floating_sel = pdb.gimp_image_floating_selection(image)
+    pdb.gimp_item_transform_flip_simple(floating_sel, 0, 1, image.height/2)
+    floating_sel = pdb.gimp_image_floating_selection(image)
+    pdb.gimp_layer_translate(floating_sel, image.width/2, 0)
+    pdb.gimp_floating_sel_anchor(floating_sel)
+    if 'trippy' in option and random.choice([0,1]):
+        drawable = pdb.gimp_image_active_drawable(image)
+        pdb.gimp_invert(drawable)
+        editLayerMask(0)
+
+
+def gridCenters(grid=[]):
+    if grid==[]:
+        grid=[4,3]
+    image = gimp.image_list()[0]
+    row_width = image.width/(grid[0])
+    columb_height = image.height/(grid[1])
+    tile_centers = [] 
+    for row in range(0,grid[0]):
+        for columb in range(0,grid[1]):
+            tile_centers.append([row_width*row+row_width/2,columb_height*columb+columb_height/2])
+    return tile_centers
+
+
+def tileMask(grid=[]):
+    image=gimp.image_list()[0]
+    layer=pdb.gimp_image_get_active_layer(image)
+    if grid==[]:
+        if image.height == image.width:
+            grid=[4,4]
+        elif image.height < image.width:
+            grid=[3,4]
+        else:
+            grid=[4,3]
+    mask = pdb.gimp_layer_create_mask(layer,0)
+    pdb.gimp_image_add_layer_mask(image, layer,mask)
+    editLayerMask(1)
+    drawable = pdb.gimp_image_active_drawable(image)
+    tile_centers=gridCenters(grid)
+    randomBrush()
+    randomDynamics()
+    for tile in tile_centers:
+        brushSize(image.width/grid[0])
+        brushColor(0,0,0)
+        editLayerMask(1)
+        pdb.gimp_paintbrush_default(drawable, len(tile), tile)
+    pdb.gimp_invert(drawable)
+    editLayerMask(0)
+
+
+def drawAkuTree(branches=6,tree_height=0, position=0):
+    from random import choice, randrange
+    image = gimp.image_list()[0]
+    drawable = pdb.gimp_image_active_drawable(image)
+    if position==0:
+        position=[]
+        position.append(randrange(image.width))
+        position.append(randrange(4*tree_height/3, 3*image.height/4))
+    if tree_height == 0:
+        tree_height=randrange(position[1]/3, position[1]-position[1]/25)
+    print 'position:' + str(position)
+    #draw trunk
+    trunk=[position[0],position[1],position[0],position[1]-tree_height]
+    trunk_size=tree_height/40+3
+    print str(trunk)
+    print 'tree_height: ' + str(tree_height)
+    print 'trunk size: ' + str(trunk_size)
+    brushSize(trunk_size)
+    pdb.gimp_paintbrush_default(drawable, len(trunk), trunk)
+    for node in range(branches):
+        node_base=[position[0],position[1]-((node*tree_height+1)/branches+tree_height/25+randrange(-1*tree_height/12,tree_height/12))]
+        base_length=tree_height/25
+        node_end=[]
+        if node%2==0:
+            node_end=[node_base[0]+base_length/2,node_base[1]-base_length/2]
+            brushSize(2*trunk_size/3)
+            pdb.gimp_paintbrush_default(drawable, 4, [node_base[0],node_base[1],node_end[0],node_end[1]])
+            brushSize(trunk_size/3)
+            pdb.gimp_paintbrush_default(drawable, 4, [node_end[0],node_end[1],node_end[0],node_end[1]-tree_height/12-(tree_height/48)])
+        else:
+            node_end=[node_base[0]-base_length/2,node_base[1]-base_length/2]
+            brushSize(2*trunk_size/3)
+            pdb.gimp_paintbrush_default(drawable, 4, [node_base[0],node_base[1],node_end[0],node_end[1]])
+            brushSize(trunk_size/3)
+            pdb.gimp_paintbrush_default(drawable, 4, [node_end[0],node_end[1],node_end[0],node_end[1]-(tree_height/12)])
+
+
+def drawAkuForest(num=25):
+    for x in range(num):
+        drawAkuTree()
+
+
 
