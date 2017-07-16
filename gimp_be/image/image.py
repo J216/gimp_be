041c1b35a1d61479c0b7e93fb5c2302bc853ee01
Jared Haer <jaredhaer@gmail.com>
2017-07-16 18:55:02 -0500

    updated create of new image and set grid spacing to be based on 12

--- gimp_be/image/image.py
+++ gimp_be/image/image.py
@@ -2,7 +2,14 @@ from gimpfu import gimp, pdb, RGB, SELECT_CRITERION_COMPOSITE
 from gimp_be.utils.string_tools import *
 
 
-def imageSetup(w=2560, h=1920):
+def imageSetup(w=0, h=0,file_name=""):
+    global settings_data
+    if file_name == "":
+        file_name='script-draw.png'
+    if w == 0:
+        w=int(settings_data['image']['width'])
+    if h == 0:
+        h=int(settings_data['image']['height'])
     """
     #Create new image from height and width
     :param w:
@@ -12,11 +19,11 @@ def imageSetup(w=2560, h=1920):
     global settings_data
     settings_data['image']['title'] = imageTitle(2)
     image = gimp.Image(w, h, RGB)
-    gridSpacing = max(image.width, image.height) / 24
+    gridSpacing = max(image.width, image.height) / 12
     pdb.gimp_image_grid_set_offset(image, image.width/2, image.height/2)
     pdb.gimp_image_grid_set_spacing(image, gridSpacing, gridSpacing)
     pdb.gimp_image_grid_set_style(image, 1)
-    settings_data['path']['export_file_name'] = str(settings_data['path']['default_save_path']) + 'script-draw.png'
+    settings_data['path']['export_file_name'] = str(settings_data['path']['default_save_path']) + file_name
     new_layer = gimp.Layer(image, "Background", image.width, image.height, 0, 100, 0)
     pdb.gimp_image_add_layer(image, new_layer, 0)
     drawable = pdb.gimp_image_active_drawable(image)
