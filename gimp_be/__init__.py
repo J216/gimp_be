64c3c80628640fd0e328db6031d1bde23ba7395e
Jared Haer <jaredhaer@gmail.com>
2017-07-16 18:58:32 -0500

    Fixed imports that were cause settings_data issue

--- gimp_be/__init__.py
+++ gimp_be/__init__.py
@@ -1,11 +1,8 @@
 from settings import *
-settings_data = loadSettings()
-updateLocationData()
-
-from utils import *
 from network import *
 from image import *
+from utils import *
 from draw import *
 from paint import *
-
 from core import *
+loadSettings()
