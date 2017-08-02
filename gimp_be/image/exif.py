769b50490a67df8cf93a57a9bbf17590f0701b15
Jared Haer <jaredhaer@gmail.com>
2017-08-02 16:32:58 -0500

    fixed issue by using call and Popen with the correct syntax for exiftool added qXDT() for quickly tagging a many tags with available info -set exiftool location in settings_data

--- gimp_be/image/exif.py
+++ gimp_be/image/exif.py
@@ -1,48 +1,62 @@
-import subprocess
 import os
 import json
-
-class ExifTool(object):
-
-    sentinel = "{ready}\n"
-
-    def __init__(self, executable = os.chdir(os.path.abspath(__file__)[0:os.path.abspath(__file__).rfind('\\')]) + "exiftool.exe"):
-        self.executable = executable
-
-    def __enter__(self):
-        self.process = subprocess.Popen(
-            [self.executable,  "-@", "-"],
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.process.stdin.write("-stay_open\nFalse\n")
-        self.process.stdin.flush()
-
-    def execute(self, *args):
-        args = args + ("-execute\n",)
-        self.process.stdin.write(str.join("\n", args))
-        self.process.stdin.flush()
-        output = ""
-        fd = self.process.stdout.fileno()
-        while not output.endswith(self.sentinel):
-            output += os.read(fd, 4096)
-        return output[:-len(self.sentinel)]
-
-    def get_metadata(self, *filenames):
-        return json.loads(self.execute("-G", "-j", "-n", *filenames))
-
+from time import sleep
+from subprocess import Popen, PIPE
+exiftool_location="c:/scr/Tools/exiftool.exe"
+from gimp_be import settings_data
+from datetime import datetime
 
 def getEXIFTags(file_name):
-    #NOT WORKING!!!! call fuctions to get json Tags
-    with ExifTool() as e:
-        metadata = e.get_metadata(file_name)
-    return metadata
+    from subprocess import Popen, PIPE
+    sleep(1)
+    p = Popen([exiftool_location, '-j',file_name], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate(b"")
+    tags=json.loads(output)[0]
+    del(p)
+    sleep(2)
+    return tags
 
-
-def setEXIFTag(file_name, tag='comment', info='GIMP Python Script Created with '):
-    #NOT WORKING!!! set tag and return true if successful
+def setEXIFTag(file_name, tag='Comment', info='8888888-8888888-8888888-888888888888'):
     from subprocess import call
-    import os
-    os.chdir(os.path.abspath(__file__)[0:os.path.abspath(__file__).rfind('\\')])
-    return call('exiftool.exe -set' + file_name + ' ' + tag + ' ' + info)
+    sleep(1)
+    cmd=exiftool_location+' -' + tag +'="'+ info.replace('"','')+'" "'+file_name+'"'
+    result=(call(cmd)==0,cmd)
+    sleep(2)
+    os.remove(file_name+'_original')
+    return result
+
+def setEXIFTags(file_name, tags={"XPComment":"Test complete1!","Comment":"Test Complete2"}):
+    from subprocess import call
+    sleep(1)
+    tag_string=""
+    for key in tags.keys():
+        tag_string=tag_string+' -' + key +'="'+ str(tags[key]).replace('"','')+'"'
+    cmd=exiftool_location+tag_string+' "'+file_name+'"'
+    print cmd
+    result=(call(cmd)==0,cmd)
+    sleep(2)
+    os.remove(file_name+'_original')
+    return result
+
+def qXDT(fn,comment=""):
+    global settings_data
+    setEXIFTags(fn,{"Copyright":settings_data['user']['author']+" "+datetime.now().strftime('%Y'),
+                    "License":settings_data['image']['license'],
+                    "Comment":comment,
+                    "XPComment":comment,
+                    "Description":comment,
+                    "ImageDescription":comment,
+                    "SEMInfo":comment,
+                    "Artist":settings_data['user']['author'],
+                    "Author":settings_data['user']['author'],
+                    "Software":"GIMP 2.8 Python 2.7 EXIFTool",
+                    "Title":comment[:comment.find('\n')],
+                    "XPTitle":comment[:comment.find('\n')],
+                    "Make":"GIMP",
+                    "Model":"Python",
+                    "Rating":"5"})
+    sleep(2)
+    try:
+        os.remove(fn+'_original')
+    except:
+        print "file already deleted"
