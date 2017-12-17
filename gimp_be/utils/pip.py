3a124eaa3af30cfaa0779790c6d7b0c9401742bb
Jared Haer <jaredhaer@gmail.com>
2017-12-16 19:23:39 -0600

    added linux friendly path for python executable from sys

--- gimp_be/utils/pip.py
+++ gimp_be/utils/pip.py
@@ -7,12 +7,12 @@ import urllib
 #install pip --windows only, I think--
 def installPIP():
     urllib.urlretrieve ("https://bootstrap.pypa.io/get-pip.py", "get-pip.py")
-    return subprocess.call(os.path.join(sys.executable, "get-pip.py"))
+    return subprocess.call(sys.executable.replace('w',''), "get-pip.py")
 
 
 #install module using PIP install, provide module name as string
 def pipInstall(module, print_out=1):
-    gimp_python = os.path.join(sys.executable, " -m pip install ", module)
+    gimp_python = sys.executable.replace('w','')+ " -m pip install "+ module
     if print_out == 1:
         print gimp_python
     return subprocess.call(gimp_python)
