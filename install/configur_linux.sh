3a77c451ec6db488e79be9a93bdf670df85bde65
Jared <jaredhaer@gmail.com>
2017-12-17 20:05:54 -0600

    initial commit

---
+++ install/configur_linux.sh
@@ -0,0 +1,17 @@
+# Copy default settings file
+sudo cp ./gimp_be-master/install/linux_default_settings.json /usr/lib/gimp/2.0/python/gimp_be/settings/settings.json
+sudo chmod 777 /usr/lib/gimp/2.0/python/gimp_be/settings/settings.json
+
+# Set host name
+
+# Set art folder
+
+# Set default save location
+
+# Set twitter api keys
+
+# Set artist name
+
+# Set licence type
+
+# Update location and weather user python script or time zone for location and fair for weather
