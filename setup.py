195defd7fc2a2efcdd68be2fe77d94c3936caa00
Jared <jaredhaer@gmail.com>
2018-02-20 12:43:39 -0600

    Add basic contents

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gimp_be',
+      version='0.1',
+      description='GIMP Basic Extension, creates easier access to GIMP features from python',
+      url='https://github.com/J216/gimp_be',
+      author='Jared Haer',
+      author_email='jaredhaer@gmail.com',
+      license='MIT',
+      packages=['gimp_be'],
+      zip_safe=False)
