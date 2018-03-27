714f221c9ef78b220fc9a92ea9a3108bc7ecf01b
Jared <jaredhaer@gmail.com>
2018-03-27 01:21:43 -0500

    Moved qXDT() to core

--- gimp_be/core.py
+++ gimp_be/core.py
@@ -4,3 +4,22 @@ from image import *
 from utils import *
 from draw import *
 from paint import *
+
+def qXDT(fn,comment=""):
+    global settings_data
+    setEXIFTags(fn,{"Copyright":settings_data['user']['author']+" "+datetime.now().strftime('%Y'),
+                    "License":settings_data['image']['license'],
+                    "Comment":comment,
+                    "XPComment":comment,
+                    "Description":comment,
+                    "ImageDescription":comment,
+                    "SEMInfo":comment,
+                    "Artist":settings_data['user']['author'],
+                    "Author":settings_data['user']['author'],
+                    "Software":"GIMP 2.8 Python 2.7 EXIFTool",
+                    "Title":comment[:comment.find('\n')],
+                    "XPTitle":comment[:comment.find('\n')],
+                    "Make":"GIMP",
+                    "Model":"Python",
+                    "Rating":"5"})
+    
