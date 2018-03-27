54f25f7b59928b33ba21782980d3fde07481ff4c
Jared <jaredhaer@gmail.com>
2018-03-27 01:20:25 -0500

    Removed qXDT(), putting it in core

--- gimp_be/image/exif.py
+++ gimp_be/image/exif.py
@@ -71,24 +71,6 @@ def decreaseRating(file_name):
             return setEXIFTag(file_name,"Rating",r)
     else:
         return setEXIFTag(file_name,"Rating","0")
-
-def qXDT(fn,comment=""):
-    global settings_data
-    setEXIFTags(fn,{"Copyright":settings_data['user']['author']+" "+datetime.now().strftime('%Y'),
-                    "License":settings_data['image']['license'],
-                    "Comment":comment,
-                    "XPComment":comment,
-                    "Description":comment,
-                    "ImageDescription":comment,
-                    "SEMInfo":comment,
-                    "Artist":settings_data['user']['author'],
-                    "Author":settings_data['user']['author'],
-                    "Software":"GIMP 2.8 Python 2.7 EXIFTool",
-                    "Title":comment[:comment.find('\n')],
-                    "XPTitle":comment[:comment.find('\n')],
-                    "Make":"GIMP",
-                    "Model":"Python",
-                    "Rating":"5"})
     sleep(1)
     if os.path.isfile(fn+'_original'):
         os.remove(fn+'_original')
