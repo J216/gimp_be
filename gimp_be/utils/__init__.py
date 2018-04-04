8e719362182db2749947f9da7fa01ab8e49dc3d3
Jared <jaredhaer@gmail.com>
2018-04-04 16:59:54 -0500

    removed unused imports

--- gimp_be/utils/__init__.py
+++ gimp_be/utils/__init__.py
@@ -1,11 +1,1 @@
-from log import *
-from metafunctions import *
-from pip import *
-from setters_getters import *
 from string_tools import *
-from study import *
-from file_management import *
-try:
-    import pip
-except:
-    installPIP()
